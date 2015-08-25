165970e20bd7f6ef3f81a3411352c4e7f11e4cd2
Anurag <anurag.agnihotri@ensuantinc.com>
2015-08-26 01:07:49 +0530

    Changes

--- designer/services/extractors/user.py
+++ designer/services/extractors/user.py
@@ -1,24 +1,21 @@
 __author__ = 'anurag'
 
 from designer.services.extractors.base import BaseExtractor
-from designer.models.user import User
-from designer.models.image import UserImage
-from designer.services.utils import convert_filters_to_query
-import json
 from bson import ObjectId
-from designer.services.utils import JSONSetEncoder
-from designer.services.utils import login_required, setup_context
 
 
 class UserExtractor(object):
 
+
     @classmethod
     def get_by_id(cls, id):
+        from designer.models.user import User
         assert isinstance(id, str) or isinstance(id, unicode) or isinstance(id, ObjectId)
         return User.objects(pk=id).first()
 
     @classmethod
     def get_by_slug(cls, slug):
+        from designer.models.user import User
         return BaseExtractor.get_by_slug(User, slug)
 
 
